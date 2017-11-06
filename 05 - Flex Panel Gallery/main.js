aff8438791448fc1ad31436c1226f6c57b564868
sylvia villegas <sylvia@svillegas.com>
2017-11-06 17:25:18 -0500

    Add '.open-active' after transition end

--- 05 - Flex Panel Gallery/main.js
+++ 05 - Flex Panel Gallery/main.js
@@ -1,9 +1,17 @@
 const panels = document.querySelectorAll('.panel');
 
 function viewPanel() {
-  this.classList.add('open', 'open-active');
+  this.classList.add('open');
 }
 
-panels.forEach( panel =>
+function animateText() {
+  this.classList.toggle('open-active');
+}
+
+panels.forEach( panel => {
   panel.addEventListener('click', viewPanel)
-);
+});
+
+panels.forEach( panel => {
+  panel.addEventListener('transitionend', animateText)
+});
