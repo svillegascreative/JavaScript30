9c1520349b3f6f6a6f6a35c0e51caa3d390bc3e7
sylvia villegas <sylvia@svillegas.com>
2017-11-06 23:57:52 -0500

    Create displayMatches, to call when search input changes

--- 06 - Type Ahead/main.js
+++ 06 - Type Ahead/main.js
@@ -1,6 +1,6 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
-
 const cities = [];
+const searchInput = document.querySelector('.search');
 
 fetch(endpoint)
   .then( response => response.json() )
@@ -12,3 +12,10 @@ function findMatches(wordToMatch, array) {
     return item.city.match(regex) || item.state.match(regex);
   );
 }
+
+function displayMatches() {
+  
+}
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
