5c89835586aa9e93f7c21a745aa8b0be71816b69
sylvia villegas <sylvia@svillegas.com>
2017-11-07 14:48:57 -0500

    Display matches in suggestions list

--- 06 - Type Ahead/main.js
+++ 06 - Type Ahead/main.js
@@ -1,6 +1,7 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 const cities = [];
 const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
 
 fetch(endpoint)
   .then( response => response.json() )
@@ -15,7 +16,15 @@ function findMatches(wordToMatch, array) {
 
 function displayMatches() {
   const matches = findMatches(this.value, cities);
-  console.log(matches);
+  const list = matches.map( item => {
+    return `
+      <li>
+        <span class="name">${item.city}, ${item.state}</span>
+        <span class="population">Pop. ${item.population}</span>
+      </li>
+    `
+  }).join('');
+  suggestions.innerHTML = list;
 }
 
 searchInput.addEventListener('change', displayMatches);
