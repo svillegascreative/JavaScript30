d41cea3cc95be15b3d56c4b9303a626f77a38657
sylvia villegas <sylvia@svillegas.com>
2017-11-08 10:46:26 -0500

    Check if at least 1 adult in people

--- 07 - Array Cardio Day 2/main.js
+++ 07 - Array Cardio Day 2/main.js
@@ -15,6 +15,10 @@
 
   // Some and Every Checks
   // Array.prototype.some() // is at least one person 19 or older?
+  const isOneAdult = people.some( person => {
+    const currentYear = (new Date()).getFullYear();
+    return currentYear - person.year >= 19)
+  });
   // Array.prototype.every() // is everyone 19 or older?
 
   // Array.prototype.find()
