c1f82374c3414fb93c421e846c994aa0858d4400
sylvia villegas <sylvia@svillegas.com>
2017-11-08 12:11:47 -0500

    Define line to stroke

--- 08 - Fun with HTML5 Canvas/main.js
+++ 08 - Fun with HTML5 Canvas/main.js
@@ -8,9 +8,15 @@ ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 
 let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
 
 function draw(e) {
   if (!isDrawing) return;
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
 }
 
 canvas.addEventListener('mousedown', () => isDrawing = true);
