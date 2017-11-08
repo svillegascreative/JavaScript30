7cafb24f0cf0326f1cde02ff4d3f068b73da493e
sylvia villegas <sylvia@svillegas.com>
2017-11-08 12:00:08 -0500

    Set up isDrawing var & listeners
    user is only drawing on mousedown

--- 08 - Fun with HTML5 Canvas/main.js
+++ 08 - Fun with HTML5 Canvas/main.js
@@ -6,3 +6,9 @@ const ctx = canvas.getContext('2d');
 ctx.strokeStyle = 'papayawhip';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+
+let isDrawing = false;
+
+canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mousedown', () => isDrawing = false);
