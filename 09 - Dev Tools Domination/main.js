3046543c681969954017cd53f7ae9e43f2f8a351
sylvia villegas <sylvia@svillegas.com>
2017-11-08 14:29:43 -0500

    Complete various single-line console f'ns

--- 09 - Dev Tools Domination/main.js
+++ 09 - Dev Tools Domination/main.js
@@ -1,28 +1,37 @@
+const p = document.querySelector('p');
 const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
 
 function makeGreen() {
-  const p = document.querySelector('p');
   p.style.color = '#BADA55';
   p.style.fontSize = '50px';
 }
 
 // Regular
+console.log('hello, world');
 
 // Interpolated
+console.log('hello, I am a %s log', 'JS');
 
 // Styled
+console.log('%c This text is large and blue', 'font-size: 40px; color: blue');
 
 // warning!
+console.warn('This is going to hurt');
 
 // Error :|
+console.error('You can\'t say that on television!');
 
-// Info
+// Info <-- not in Chrome?
+console.info('Fun fact: barnacles have the longest penis relative to body size.');
 
 // Testing
+console.assert( 'header' == '.header', 'Interview fail!');
 
 // clearing
+console.clear();
 
 // Viewing DOM Elements
+console.dir(p);
 
 // Grouping together
 
