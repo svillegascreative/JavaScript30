8e5955ef1b2aa6abec50484e702558571282fa7f
sylvia villegas <sylvia@svillegas.com>
2017-11-08 11:53:30 -0500

    Define stroke & line styles

--- 08 - Fun with HTML5 Canvas/main.js
+++ 08 - Fun with HTML5 Canvas/main.js
@@ -1,5 +1,8 @@
 const canvas = document.querySelector('#draw');
-const ctx = canvas.getContext('2d');
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext('2d');
+ctx.strokeStyle = 'papayawhip';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
