16da3c2d3870cb8b52cd465a13ded2cb5a71d2e4
sylvia villegas <sylvia@svillegas.com>
2017-11-08 12:30:22 -0500

    Change hue while drawing

--- 08 - Fun with HTML5 Canvas/main.js
+++ 08 - Fun with HTML5 Canvas/main.js
@@ -3,21 +3,29 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext('2d');
-ctx.strokeStyle = 'papayawhip';
+// ctx.strokeStyle = 'papayawhip';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
 
 function draw(e) {
   if (!isDrawing) return;
+
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
   ctx.moveTo(lastX, lastY);
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.stroke();
+
   [lastX, lastY] = [e.offsetX, e.offsetY];
+  hue++;
+  if (hue >= 360) {
+    hue = 0;
+  }
 }
 
 canvas.addEventListener('mousedown', (e) => {
