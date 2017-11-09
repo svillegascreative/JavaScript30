75ab9f842086ebca780fb1fb7dd44e54a768539c
sylvia villegas <sylvia@svillegas.com>
2017-11-09 09:54:55 -0500

    Keep track of last checked box

--- 10 - Hold Shift and Check Checkboxes/main.js
+++ 10 - Hold Shift and Check Checkboxes/main.js
@@ -1,1 +1,10 @@
+// const items = document.querySelectorAll('.item');
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
+let lastChecked;
+
+function setLastChecked(e) {
+  lastChecked = this;
+}
+
+checkboxes.forEach( checkbox => checkbox.addEventListener('click', setLastChecked) );
