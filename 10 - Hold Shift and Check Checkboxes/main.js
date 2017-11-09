cea6dd77911f794ca849b159ab9914d9304bc9cb
sylvia villegas <sylvia@svillegas.com>
2017-11-09 13:29:21 -0500

    Get Index of lastChecked

--- 10 - Hold Shift and Check Checkboxes/main.js
+++ 10 - Hold Shift and Check Checkboxes/main.js
@@ -2,10 +2,12 @@
 const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 const checkboxArray = Array.from(checkboxes);
 
-let lastChecked;
+let lastChecked,
+  lastCheckedIndex;
 
-function setLastChecked(e) {
+function setLastChecked() {
   lastChecked = this;
+  lastCheckedIndex = checkboxArray.indexOf(this);
 }
 
 checkboxes.forEach( checkbox => checkbox.addEventListener('click', setLastChecked) );
