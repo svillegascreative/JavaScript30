2d8ade4d3e6a8f0d45211b4c457549afa723166a
sylvia villegas <sylvia@svillegas.com>
2017-11-09 14:46:32 -0500

    Create vars for elements to be manipulated

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,7 @@
+const player = document.querySelector('.player');
+const video = document.querySelector('.viewer');
+const progress = document.querySelector('.progress');
+const progressFilled = document.querySelector('.progress__filled');
+const toggle = document.querySelector('.toggle');
+const slider = document.querySelectorAll('.player__slider');
+const skipButtons = document.querySelectorAll('button[data-skip]');
