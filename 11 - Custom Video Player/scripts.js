5c0974883a7f0bf37c90be42c9dce1741f6350ec
sylvia villegas <sylvia@svillegas.com>
2017-11-10 15:34:11 -0500

    Add listeners & callback f'n for sliders

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -3,7 +3,7 @@ const video = document.querySelector('.viewer');
 const progress = document.querySelector('.progress');
 const progressFilled = document.querySelector('.progress__filled');
 const toggle = document.querySelector('.toggle');
-const slider = document.querySelectorAll('.player__slider');
+const sliders = document.querySelectorAll('.player__slider');
 const skipButtons = document.querySelectorAll('button[data-skip]');
 
 function togglePlay() {
@@ -23,7 +23,16 @@ function skipPlay() {
   video.currentTime += parseFloat(skipTime);
 }
 
-
+function handleSlider() {
+  console.log('handled');
+  if (this.name == 'volume') {
+    video.volume = this.value;
+    console.log(this.name);
+  }
+  if (this.name == 'playbackRate') {
+    video.playbackRate = this.value;
+  }
+}
 
 toggle.addEventListener('click', togglePlay);
 window.addEventListener('keypress', function(e) {
@@ -36,3 +45,5 @@ video.addEventListener('play', togglePlayButton);
 video.addEventListener('pause', togglePlayButton);
 
 skipButtons.forEach( button => button.addEventListener('click', skipPlay) );
+
+sliders.forEach( slider => slider.addEventListener('change', handleSlider) );
