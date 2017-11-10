4849d8012ef01e2cfcb0dfbb9d34bc92b075cef0
sylvia villegas <sylvia@svillegas.com>
2017-11-10 09:06:50 -0500

    Toggle play when button clicked

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -5,3 +5,17 @@ const progressFilled = document.querySelector('.progress__filled');
 const toggle = document.querySelector('.toggle');
 const slider = document.querySelectorAll('.player__slider');
 const skipButtons = document.querySelectorAll('button[data-skip]');
+
+let isPlaying = false;
+
+function togglePlay() {
+  if (isPlaying) {
+    video.pause();
+    isPlaying = false;
+  } else {
+    video.play();
+    isPlaying = true;
+  }
+}
+
+toggle.addEventListener('click', togglePlay);
