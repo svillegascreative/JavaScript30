a8760cf7cb646b3512940a9f1d2efb3b9fefa6b3
sylvia villegas <sylvia@svillegas.com>
2017-11-13 12:42:29 -0500

    Return keypress array of same length as secret code

--- 12 - Key Sequence Detection/main.js
+++ 12 - Key Sequence Detection/main.js
@@ -1,8 +1,9 @@
+const secretCode = 'liberty';
 const pressed = [];
 
 function compareToSecret(e) {
   pressed.push(e.key);
-  console.log(pressed);
+  pressed.splice(-secretCode - 1, pressed.length - secretCode.length);
 }
 
 window.addEventListener('keypress', compareToSecret);
