2ec4a8473f6fbe9c473188190c9869941b98f5c2
sylvia villegas <sylvia@svillegas.com>
2017-11-13 12:43:06 -0500

    Compare keypress array to secret code

--- 12 - Key Sequence Detection/main.js
+++ 12 - Key Sequence Detection/main.js
@@ -4,6 +4,9 @@ const pressed = [];
 function compareToSecret(e) {
   pressed.push(e.key);
   pressed.splice(-secretCode - 1, pressed.length - secretCode.length);
+  if (pressed.join("") == secretCode) {
+    console.log('you did it!');
+  }
 }
 
 window.addEventListener('keypress', compareToSecret);
