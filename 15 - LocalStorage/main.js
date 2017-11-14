8a96cb0625ee7cb9505436f68d94fd84e963d3a3
sylvia villegas <sylvia@svillegas.com>
2017-11-14 09:53:53 -0500

    Add form data to items array

--- 15 - LocalStorage/main.js
+++ 15 - LocalStorage/main.js
@@ -1,8 +1,10 @@
 const addItems = document.querySelector('.add-items');
+const input = document.querySelector('input[name="item"]');
 const itemsList = document.querySelector('.plates');
 const items = [];
 
 function addItem() {
+  items.push(input.value);
 }
 
 addItems.addEventListener('submit', addItem);
