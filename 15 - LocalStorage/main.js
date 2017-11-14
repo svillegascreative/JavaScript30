2e1de4f01414de9e05b9bb6a759371a05570013e
sylvia villegas <sylvia@svillegas.com>
2017-11-14 09:53:22 -0500

    Create addItem, as callback for form submit

--- 15 - LocalStorage/main.js
+++ 15 - LocalStorage/main.js
@@ -1,3 +1,8 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = [];
+
+function addItem() {
+}
+
+addItems.addEventListener('submit', addItem);
