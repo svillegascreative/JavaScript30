940d8e30e902ff968c79c27992879f245bb16c0f
sylvia villegas <sylvia@svillegas.com>
2017-11-14 14:10:26 -0500

    Display items (from localStorage) at runtime

--- 15 - LocalStorage/main.js
+++ 15 - LocalStorage/main.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(e) {
   e.preventDefault();
@@ -27,3 +27,5 @@ function displayItems() {
 }
 
 addItems.addEventListener('submit', addItem);
+
+displayItems();
