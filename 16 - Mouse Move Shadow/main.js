6a7824deca3b2e79ac8de10d4dcf2484831fcfc7
sylvia villegas <sylvia@svillegas.com>
2017-11-15 09:17:14 -0500

    Get mouse position from mousemove event

--- 16 - Mouse Move Shadow/main.js
+++ 16 - Mouse Move Shadow/main.js
@@ -2,6 +2,10 @@ const hero = document.querySelector('.hero');
 const header = hero.querySelector('h1');
 
 function moveShadow(e) {
+  const height = hero.offsetHeight;
+  const width = hero.offsetWidth;
+  let mouseX = e.offsetX;
+  let mouseY = e.offsetY;
 }
 
 hero.addEventListener('mousemove', moveShadow);
