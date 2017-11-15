f4b74882649def06a669317a3fc34ed88871c575
sylvia villegas <sylvia@svillegas.com>
2017-11-15 09:17:34 -0500

    Normalize mouse pos'n over h1

--- 16 - Mouse Move Shadow/main.js
+++ 16 - Mouse Move Shadow/main.js
@@ -6,6 +6,12 @@ function moveShadow(e) {
   const width = hero.offsetWidth;
   let mouseX = e.offsetX;
   let mouseY = e.offsetY;
+
+  if (this !== e.target) {
+    mouseX += e.target.offsetLeft;
+    mouseY += e.target.offsetTop;
+  }
+  console.log(mouseX, mouseY);
 }
 
 hero.addEventListener('mousemove', moveShadow);
