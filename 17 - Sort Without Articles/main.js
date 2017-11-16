85f80339f26782dc3f741a88574b6d769258fbb4
sylvia villegas <sylvia@svillegas.com>
2017-11-16 11:29:28 -0500

    Create function to remove articles from band names

--- 17 - Sort Without Articles/main.js
+++ 17 - Sort Without Articles/main.js
@@ -1,5 +1,9 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
+function removeArticles(name) {
+  return name.replace(/^(a |the |an )/i, '');
+}
+
 const sortedBands = bands.sort();
 
 console.log(sortedBands);
