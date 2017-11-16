17f7b79305549c4dd66dce8ed1b18f2b427d3f0a
sylvia villegas <sylvia@svillegas.com>
2017-11-16 11:49:06 -0500

    Add sorted bands to HTML list

--- 17 - Sort Without Articles/main.js
+++ 17 - Sort Without Articles/main.js
@@ -1,3 +1,4 @@
+const bandList = document.querySelector('#bands');
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
 function removeArticles(name) {
@@ -7,3 +8,6 @@ function removeArticles(name) {
 const sortedBands = bands.sort( (a, b) => {
   return removeArticles(a) > removeArticles(b) ? 1 : -1;
 });
+
+const listedBands = sortedBands.map( band => `<li>${band}</li>`).join('');
+bandList.innerHTML = listedBands;
